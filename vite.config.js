dbaa7d7727149d04687c4bcc6dd29efb10b5fe7c
fuadadhim24 <fuadadhim24@gmail.com>
2025-07-21 13:43:54 +0700

    fix: dockerize laravel vilt app

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,14 @@ export default defineConfig({
             ),
         },
     },
+    server: {
+        host: true,
+        port: 5173,
+        strictPort: true,
+        watch: {
+            usePolling: true,
+        },
+    },
     plugins: [
         tailwindcss(),
         laravel({
